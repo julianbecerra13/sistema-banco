93903cc41d7416809d6bcf3cf7c801d358d38cf5
DARKPERRI <73130568+julianbecerra13@users.noreply.github.com>
2022-05-31 00:20:16 -0500

    Add files via upload
    mejoras visuales en la interfaz y en las funciones

--- banco imperial/cajero.py
+++ banco imperial/cajero.py
@@ -6,19 +6,24 @@ BD = []
 
 # FUNCIONES
 def registrar():
-    nombre = input("ingrese su nombre: ")
-    apellido = input("ingrese su apellido: ")
-    cedula = input("ingrese su cedula: ")
-    edad = input("ingrese su edad: ")
+    nombre = input("ingrese nombres: ")
+    apellido = input("ingrese apellidos: ")
+    cedula = input("ingrese numero de identificacion: ")
+    edad = int(input("ingrese su edad: "))
+
+    while edad >= 99:
+        print("ingrese una edad valida")
+        edad = int(input("ingrese su edad: "))
+
     fecha = datetime.datetime.now()
-    saldo = int(input("ingrese su saldo: "))
+    saldo = int(input("ingrese su saldo inicial: "))
     prestamos = 0
 
-    if saldo > 1000:
+    if saldo >= 50000:
         print("usted ha sido aprobado")
 
     else:
-        print("ingrese un saldo mayor a 1000")
+        print("ingrese un saldo mayor o igual a 50.000")
         saldo = int(input("ingrese su saldo: "))
 
     cuenta = [nombre, apellido, cedula, edad, fecha, saldo, prestamos]
@@ -34,31 +39,36 @@ def ingresar(cuenta):
 
 
 def depositar(cuenta):
-    for i in BD:
-        if i[2] == cuenta:
+    for cuenta in BD:
+        if cuenta[2] == cedula:
             deposito = int(input("ingrese el monto a depositar: "))
-            if deposito <= i[6]:
-                i[6] -= deposito
+            if deposito <= cuenta[6]:
+                cuenta[6] -= deposito
             else:
-                i[5] += deposito - i[6]
-                i[6]= 0
-
-            print("usted tiene un prestamo pendiente de: ", i[6])
-            print("Saldo actual: ", i[5])
+                cuenta[5] += deposito - cuenta[6]
+                cuenta[6] = 0
+            print("deposito exitoso")
+            print("Saldo actual: ", cuenta[5])
+            print("usted tiene un prestamo pendiente de: ", cuenta[6])
 
 
 def retirar(cuenta):
     for i in BD:
         if i[2] == cuenta:
             saldo = int(input("ingrese el monto a retirar: "))
-            
+
             if saldo > i[5]:
                 print("saldo insuficiente")
+            elif saldo == 50000:
+                print("no puede retirar su saldo inicial")
+            elif i[6] > 0:
+                print("no puede retirar hasta que el prestamo sea pagado")
             else:
                 i[5] -= saldo
                 print("retiro exitoso")
 
-            print("Saldo actual: ", i[5])
+            print("su Saldo actual es de: ", i[5])
+            print("usted tiene un prestamo pendiente de: ", i[6])
             return True
     return False
 
@@ -66,9 +76,10 @@ def retirar(cuenta):
 def consultar(cedula):
     for cuenta in BD:
         if cuenta[2] == cedula:
-            print("Bienvenido", cuenta[0], cuenta[1])
-            print("Saldo actual: ", cuenta[5])
-            print("prestamo a deber: ", cuenta[6])
+            print("usuario: ", cuenta[0], cuenta[1])
+            print(" su Saldo actual es de : ", cuenta[5])
+            print(" su prestamo a deber es de : ", cuenta[6])
+
             return True
     return False
 
@@ -81,6 +92,7 @@ def prestamo(cedula):
             if cuenta[6] == 0:
                 prestamo = int(input("ingrese valor a solicitar prestado: "))
                 cuenta[6] = prestamo
+                print("prestamo aprobado")
             else:
                 print("ya tienes un prestamo pendiente:", cuenta[6])
 
@@ -93,40 +105,44 @@ opcion = -100
 while opcion != 0:
     print(
         '''
-            ==============================================================
-            =                      BANCO IMPERIAL                         =
-            ===============================================================
-            ===============================================================
-            =                       1 CREAR CUENTA                        =
-            =                       2 INGRESAR                            =
-            =============             0 salir                   ===========
-            ===============================================================
+                    |===============================================================|
+                    |=                      BANCO IMPERIAL                         =|
+                    |===============================================================|
+                    |===============================================================|
+                    |=                       1 CREAR CUENTA                        =|
+                    |=                       2 INGRESAR                            =|
+                    |===============================================================|
+                    |=============             0 salir                   ===========|
+                    |===============================================================|
         ''')
     opcion = int(input("opcion: "))
 
-    # FIN DE INTERFAZ PRINCIPAL
+ # FIN DE INTERFAZ PRINCIPAL
 
     if opcion == 1:
         registrar()
 
     elif opcion == 2:
         cuenta = input("ingrese numero de cedula: ")
-
+# INTERFAZ DE INGRESO
         if ingresar(cuenta):
             op = -100
             while op != 0:
-                print("===============================================================")
-                print("=                      BANCO IMPERIAL                         =")
-                print("===============================================================")
-                print("===============================================================")
-                print("=                    ESCOJA SU MOVIMIENTO                     =")
-                print("=                       1 DEPOSITAR                           =")
-                print("=                       2 RETIRAR                             =")
-                print("=                       3 PRESTAMO                            =")
-                print("=                    4 CONSULTAR SALDO                        =")
-                print("===============================================================")
-                print("=============           0 regresar                =============")
-                print("===============================================================")
+
+                print('''
+                    |===============================================================|
+                    |=                      BANCO IMPERIAL                         =|
+                    |===============================================================|
+                    |===============================================================|
+                    |=                    ESCOJA SU MOVIMIENTO                     =|
+                    |=                       1 DEPOSITAR                           =|
+                    |=                       2 RETIRAR                             =|
+                    |=                       3 PRESTAMO                            =|
+                    |=                    4 CONSULTAR SALDO                        =|
+                    |===============================================================|
+                    |=============           0 regresar                =============|
+                    |===============================================================|
+                    ''')
                 op = int(input("opcion: "))
                 if op == 1:
 
@@ -144,6 +160,8 @@ while opcion != 0:
                     print("opcion invalida")
         else:
             print("cuenta no encontrada")
+# FIN DE INTERFAZ DE INGRESO
+
     else:
         print("opcion invalida")
 
